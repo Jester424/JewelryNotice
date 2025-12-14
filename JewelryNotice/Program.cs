8d05bf060cf8bd1f0753db4e0f66af3bc0e806d6
Nicholas Nicosia <nnicosi@wgu.edu>
2025-12-13 20:08:54 -0800

    Removed unnecessary using statements

--- JewelryNotice/Program.cs
+++ JewelryNotice/Program.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using static System.Net.WebRequestMethods;
-using Newtonsoft.Json;
+﻿using System.Text.Json;
 using Microsoft.Toolkit.Uwp.Notifications;
 
 
